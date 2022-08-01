4b10df669bdd4af795761f0423dad8c724c038b8
juanBailon <juanojo2001.5@gmail.com>
2022-08-01 16:59:21 -0500

    anadiendo CORS en index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 const port = 4000;
 const taskRoutes = require('./routes/task.routes');
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors());
 
 app.use(taskRoutes);
 
